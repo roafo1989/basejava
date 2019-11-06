80f720111230b0d6718b541e64aa0e3d3c0ee2d4
Roman Afonin <roafo@mail.ru>
2019-11-06 16:30:27 +0300

    HW0

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,9 @@
+import com.sun.deploy.util.ArrayUtil;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +11,47 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for(int i = 0; i < storage.length; i++) storage[i] = null;
     }
 
     void save(Resume r) {
+        for(int i = 0; i < storage.length; i++){
+            if(storage[i]==null){
+                storage[i] = r;
+                return;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume r = null;
+        for (Resume resume : storage) {
+            if (resume.uuid.equals(uuid)) r = resume;
+            break;
+        }
+        return r;
     }
 
     void delete(String uuid) {
+       for(int i = 0; i < storage.length; i++){
+           if(storage[i].uuid.equals(uuid)) {
+               for(int k = i; k < storage.length; k++){
+                   if(storage[k] != null){
+                       storage[k] = storage[k+1];
+                   } else return;
+               }
+           }
+       }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return storage;
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -33,7 +33,6 @@ public class MainTestArrayStorage {
     static void printAll() {
         System.out.println("\nGet All");
         for (Resume r : ARRAY_STORAGE.getAll()) {
-            System.out.println(r);
         }
     }
 }
