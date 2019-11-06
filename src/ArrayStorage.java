223ebca9a0bc8ed72b51541a1e78b9ca7ce40530
Roman Afonin <roafo@mail.ru>
2019-11-07 00:55:15 +0300

    fixed after 1 feedback

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,10 +6,10 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        for(int i = 0; i < storage.length; i++) {
-            if(storage[i] != null){
-                storage[i] = null;
-            } else return;
+        int i = 0;
+        while (storage[i] != null){
+            storage[i] = null;
+            i++;
         }
     }
 
@@ -34,17 +34,17 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int k = 0;
-        while (storage[k] != null){
-            if(storage[k].uuid.equals(uuid)){
-                int i = k;
-                while (storage[i] != null){
-                    storage[i] = storage[i+1];
-                    i++;
+        int i = 0;
+        while (storage[i] != null){
+            if(storage[i].uuid.equals(uuid)){
+                int k = i;
+                while (storage[k] != null){
+                    storage[k] = storage[k+1];
+                    k++;
                 }
                 return;
             }
-            k++;
+            i++;
         }
     }
 
